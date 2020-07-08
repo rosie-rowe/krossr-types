6b8cae253c0d0ed25186d5b23b20c93ba45fbdf7
Rosalyn Rowe <rrowe404@gmail.com>
2020-07-07 23:59:51 -0400

    export LevelListFilterOptions from here instead of keeping on client

---
+++ src/LevelList/LevelListFilterOptions.ts
@@ -0,0 +1,6 @@
+export interface LevelListFilterOptions {
+    sizeRestriction?: string;
+    searchText?: string;
+    sortBy?: string;
+    sortDirection?: string
+}
--- src/LevelList/LevelListModule.ts
+++ src/LevelList/LevelListModule.ts
@@ -1,2 +1,3 @@
+export * from './LevelListFilterOptions';
 export * from './LevelListLevelViewModel';
 export * from './LevelListViewModel';
