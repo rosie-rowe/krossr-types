b9ceacbf598c597bf110a961ffdd28b210960b87
Rosalyn Rowe <rrowe404@gmail.com>
2020-07-07 20:40:55 -0400

    add a viewModel for password reset token validation

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ export * from './src/Error/ErrorModule';
 export * from './src/Level/LevelModule';
 export * from './src/LevelList/LevelListModule';
 export * from './src/Pagination/PaginationModule';
+export * from './src/Password/PasswordModule';
 export * from './src/Rating/RatingModule';
 export * from './src/Router/RouterModule';
 export * from './src/User/UserModule';
---
+++ src/Password/ResetValidationViewModel.ts
@@ -0,0 +1,3 @@
+export interface ResetValidationViewModel {
+    valid: boolean;
+}
